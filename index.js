a89b48b6e687b157d4cb137fd0de645959368f4e
ilyassfas123 <ilyassfaskaa123@gmail.com>
2023-02-28 16:12:00 +0100

    bj

---
+++ index.js
@@ -0,0 +1,16 @@
+var tableau = [ 10 , 120 , 3 , 78 ];
+
+var tableau1 = [ 31 , 20 ]
+
+
+
+
+for (var i = 0; i < tableau.length; i++) {
+
+
+for (var j = 0; i < tableau1.length; i++) {
+    tableau[i] = tableau [i] + tableau1[j]
+
+}
+}
+console.log(tableau)
